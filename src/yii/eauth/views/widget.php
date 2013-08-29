a4794fa94e89d3f9c5bd16deefea412b5b65381a
Nodge <nodge@yandex.ru>
2013-08-30 00:15:56 +0600

    Add missing views

---
+++ src/yii/eauth/views/widget.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\base\View;
+
+/** @var $this View */
+/** @var $id string */
+/** @var $services stdClass[] See EAuth::getServices()*/
+/** @var $action string */
+/** @var $popup bool */
+/** @var $assetBundle string Alias to AssetBundle */
+
+Yii::createObject(array(
+	'class' => $assetBundle,
+))->register($this);
+
+// Open the authorization dilalog in popup window.
+if ($popup) {
+	$options = array();
+	foreach ($services as $name => $service) {
+		$options[$service->id] = $service->jsArguments;
+	}
+	$this->registerJs('$("#'.$id.'").eauth('.json_encode($options).');');
+}
+
+?><div class="eauth" id="<?php echo $id; ?>">
+	<ul class="eauth-list">
+	  <?php
+		foreach ($services as $name => $service) {
+			echo '<li class="eauth-service eauth-service-id-' . $service->id . '">';
+			echo Html::a($service->title, array($action, 'service' => $name), array(
+				'class' => 'eauth-service-link',
+				'data-eauth-service' => $service->id,
+			));
+			echo '</li>';
+		}
+	  ?>
+	</ul>
+</div>
