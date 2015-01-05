4145ec5247f93cf8f5f443886f7d230d0b57e89a
mipapo <michael@mipapo.de>
2015-01-05 10:58:39 +0100

    German translation

---
+++ src/messages/de/eauth.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+	'Undefined service name: {service}.' => 'Undefinierter Servicename: {service}',
+	'Invalid response http code: {code}.' => 'Ungültiger HTTP Code: {code}',
+	'Invalid response format.' => 'Ungültiges Antwortformat.',
+	'Unable to complete the authentication because the required data was not received.' => 'Die Authentifizierung konnte nicht durchgeführt werden, da keine Daten empfangen wurden.',
+	'Unable to complete the request because the user was not authenticated.' => 'Die Anfrage konnte nicht durchgeführt werden, da der Nutzer nicht authentifiziert war.',
+
+	'Redirecting back to the application...' => 'Weiterleitung zur Applikation...',
+	'Click here to return to the application.' => 'Klicke hier um zur Applikation zurückzukehren',
+
+	'Google' => 'Google',
+	'Twitter' => 'Twitter',
+	'Yandex' => 'Yandex',
+	'VK.com' => 'VK.com',
+	'Facebook' => 'Facebook',
+	'Mail.ru' => 'Mail.ru',
+	'Moikrug.ru' => 'Moikrug.ru',
+	'Odnoklassniki' => 'Odnoklassniki',
+	'LinkedIn' => 'LinkedIn',
+	'GitHub' => 'GitHub',
+);
