f236143ad0bf2060f7872a821c65ea330e1c57c1
Ghitu Ilie-Alin <alin@updivision.com>
2017-04-09 23:21:12 +0300

    Added Romanian language (#94)

---
+++ src/messages/ro/eauth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+	'Undefined service name: {service}.' => 'Serviciu nedefinit: {service}.',
+	'Invalid response http code: {code}.' => 'Codul http al răspunsului este invalid: {code}.',
+	'Invalid response format.' => 'Formatul răspunsului este invalid.',
+	'Unable to complete the authentication because the required data was not received.' => 'Nu s-a putut finaliza autentificarea pentru că datele necesare nu au fost primite.',
+	'Unable to complete the request because the user was not authenticated.' => 'Cererea nu a fost finalizată pentru că utilizatorul nu este autentificat.',
+
+	'Redirecting back to the application...' => 'Te redirecționăm înapoi la aplicație...',
+	'Click here to return to the application.' => 'Apasă aici pentru a te întoarce la aplicație.',
+
+	'Google' => 'Google',
+	'Twitter' => 'Twitter',
+	'Yandex' => 'Yandex',
+	'VK.com' => 'VK.com',
+	'Facebook' => 'Facebook',
+	'Mail.ru' => 'Mail.ru',
+	'Moikrug.ru' => 'Moikrug.ru',
+	'Odnoklassniki' => 'Odnoklassniki',
+	'LinkedIn' => 'LinkedIn',
+	'GitHub' => 'GitHub',
+];
