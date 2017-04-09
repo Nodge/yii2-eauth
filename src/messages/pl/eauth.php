0cc27941cdd8ee64434b194f7ac7df877facba5d
Bizley <pawel@positive.codes>
2017-04-09 22:20:50 +0200

    PL translation (#93)

---
+++ src/messages/pl/eauth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'Undefined service name: {service}.' => 'Nieznana nazwa usługi: {service}.',
+    'Invalid response http code: {code}.' => 'Nieprawidłowy kod HTTP odpowiedzi: {code}.',
+    'Invalid response format.' => 'Nieprawidłowy format odpowiedzi.',
+    'Unable to complete the authentication because the required data was not received.' => 'Nie można dokonać uwierzytelniania, ponieważ nie otrzymano wymaganych danych.',
+    'Unable to complete the request because the user was not authenticated.' => 'Nie można przetworzyć żądania, ponieważ użytkownik nie został uwierzytelniony.',
+
+    'Redirecting back to the application...' => 'Przekierowuję z powrotem do aplikacji...',
+    'Click here to return to the application.' => 'Kliknij tutaj, aby powrócić do aplikacji.',
+
+    'Google' => 'Google',
+    'Twitter' => 'Twitter',
+    'Yandex' => 'Yandex',
+    'VK.com' => 'VK.com',
+    'Facebook' => 'Facebook',
+    'Mail.ru' => 'Mail.ru',
+    'Moikrug.ru' => 'Moikrug.ru',
+    'Odnoklassniki' => 'Odnoklassniki',
+    'LinkedIn' => 'LinkedIn',
+    'GitHub' => 'GitHub',
+];
